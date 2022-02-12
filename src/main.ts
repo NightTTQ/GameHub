2d0d5f59495d29e21e46381474e72a8ea280a3c4
TTQ <1932170807@qq.com>
2022-02-12 22:35:18 +0800

    add element-ui

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
+app.use(ElementPlus);
 
-app.mount('#app')
+app.mount("#app");
