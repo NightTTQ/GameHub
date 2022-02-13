fcdb99229d32dd9d74e0b7bfa2a4bf4234b029b8
TTQ <1932170807@qq.com>
2022-02-13 21:53:12 +0800

    add gamewall

---
+++ src/components/GameWall.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-container>
+    <el-aside>Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.el-aside {
+  min-width: 200px;
+  background-color: #d3dce6;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  text-align: center;
+  line-height: 160px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import GameWall from "@/components/GameWall.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <GameWall />
   </main>
 </template>
