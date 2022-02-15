f94a754f6e5f887f85ad1d38facad405a45bc690
TTQ <1932170807@qq.com>
2022-02-15 18:31:33 +0800

    select checkbox tree ver

--- src/components/GameWall.vue
+++ src/components/GameWall.vue
@@ -2,44 +2,60 @@
   <el-container>
     <el-aside>
       <h4>筛选</h4>
-      <el-collapse>
-        <el-checkbox-group @change="handleCheckAllChange" v-model="groups">
-          <el-collapse-item v-for="(items, key) in gameClass">
-            <template #title>
-              <el-checkbox :label="key">
-                {{ key }}
-              </el-checkbox>
-            </template>
-            <el-checkbox-group @change="handleCheckedTagsChange" v-model="tags">
-              <el-checkbox v-for="item in items" :label="item">
-                {{ item }}
-              </el-checkbox>
-            </el-checkbox-group>
-          </el-collapse-item>
-        </el-checkbox-group>
-      </el-collapse>
+      <el-tree
+        ref="treeRef"
+        :data="treeData"
+        show-checkbox
+        :props="props"
+        :expand-on-click-node="false"
+        @check-change="handleCheckedTagsChange"
+      />
     </el-aside>
     <el-main>Main</el-main>
   </el-container>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import gameClass from "@/assets/gameClass.json";
+import type { ElTree } from "element-plus";
 
+onMounted(() => handleTreeData());
+
+const treeData = ref();
+const treeRef = ref<InstanceType<typeof ElTree>>();
 const tags = ref();
-const groups = ref();
 
-const handleCheckedTagsChange = (value: string[]) => {
-  console.log(value);
+const props = {
+  label: "class",
+  children: "children",
 };
-const handleCheckAllChange = (value: string[]) => {
-  console.log(value);
-  //checkedCities.value = value ? cities : []
-  //isIndeterminate.value = false
+
+const handleTreeData = () => {
+  let _data = [];
+  for (const item in gameClass) {
+    let tmp = {
+      class: "",
+      children: [{}],
+    };
+    tmp.children.pop();
+    tmp.class = gameClass[item]["class"];
+    for (const _item in gameClass[item]["children"]) {
+      tmp.children.push({ class: gameClass[item]["children"][_item] });
+    }
+    _data.push(tmp);
+  }
+  treeData.value = _data;
 };
-const test = (msg: string) => {
-  console.log(msg);
+const handleCheckedTagsChange = (event: any, status: boolean) => {
+  const selected = treeRef.value?.getCheckedNodes();
+  let tmp = [];
+  for (const item in selected) {
+    if (!selected[Number(item)].children) {
+      tmp.push(selected[Number(item)].class);
+    }
+  }
+  tags.value = tmp;
 };
 </script>
 
@@ -53,10 +69,10 @@ const test = (msg: string) => {
 h4 {
   font-size: 18px;
   padding-left: 8px;
+  padding-bottom: 18px;
+  border-bottom: 1px solid #eff1f1;
 }
-.el-checkbox-group {
-  line-height: 1;
-}
+
 .el-main {
   background-color: #e9eef3;
   text-align: center;
