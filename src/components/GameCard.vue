5bcba13c7ca9ca0545f08a1fb82f45c66299fb7c
TTQ <1932170807@qq.com>
2022-02-20 23:38:52 +0800

    GameCard layout

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -1,46 +1,85 @@
 <template>
-  <el-card shadow="hover" :body-style="{ padding: '0px' }">
-    <img
-      src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-      class="image"
-    />
-    <div style="padding: 14px">
-      <span>Yummy hamburger</span>
+  <el-card shadow="hover" :body-style="{ padding: '0px' }" @click="onClick">
+    <div class="image-div" :style="imageURL"></div>
+
+    <div style="padding: 12px">
       <div class="bottom">
-        <time class="time">{{ currentDate }}</time>
-        <el-button type="text" class="button">Operating</el-button>
+        <div class="name-div">
+          <h4 style="margin: 0">{{ name }}</h4>
+        </div>
+        <div class="author-div">
+          <span v-for="item of author">{{ item.name }}</span>
+        </div>
+        <div class="class-div">
+          <span>{{ class }}</span>
+        </div>
       </div>
     </div>
   </el-card>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import type { ComputedRef } from "vue";
 
-const currentDate = ref(new Date());
+export interface Author {
+  name: string;
+  type: number;
+  uid: string;
+}
+
+const props = defineProps<{
+  name?: string;
+  image?: string;
+  author?: Array<Author>;
+  class?: string;
+}>();
+const imageURL: ComputedRef<string> = computed(
+  () => "background-image: url(" + props.image + ")"
+);
+
+const onClick = (event: any) => {
+  console.log(props.name);
+};
 </script>
 
 <style>
-.time {
-  font-size: 13px;
-  color: #999;
+.el-card {
+  display: inline-block;
+  cursor: pointer;
 }
 
 .bottom {
-  margin-top: 13px;
-  line-height: 12px;
+  line-height: 1;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  flex-direction: row;
+  flex-wrap: wrap;
 }
 
-.button {
-  padding: 0;
-  min-height: auto;
+.image-div {
+  background-size: cover;
+  background-position-x: center;
+  background-position-y: center;
+  height: 0%;
+  padding-top: calc(9 / 16 * 100%);
 }
-
-.image {
+.name-div {
   width: 100%;
-  display: block;
+  text-align: left;
+  line-height: 1.2;
+}
+.author-div {
+  width: 50%;
+  text-align: left;
+  line-height: 1.2;
+  font-size: 1em;
+  opacity: 0.8;
+}
+.class-div {
+  width: 50%;
+  text-align: right;
+  line-height: 1.2;
+  font-size: 0.8em;
+  opacity: 0.8;
 }
 </style>
