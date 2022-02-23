58c2194a44aae5e6842655c8b8d8b12953fbf634
TTQ <1932170807@qq.com>
2022-02-23 22:33:09 +0800

    add el-scrollbar and el-backtop

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <el-menu
-    :default-active="activeIndex"
-    mode="horizontal"
-    background-color="#424242"
-    text-color="#fff"
-    active-text-color="#ffd04b"
-    router
-  >
-    <el-menu-item index="/">Home</el-menu-item>
-    <el-menu-item index="/about">About</el-menu-item>
-  </el-menu>
+  <el-scrollbar>
+    <el-backtop target=".el-scrollbar .el-scrollbar__wrap"></el-backtop>
+    <el-menu
+      :default-active="activeIndex"
+      mode="horizontal"
+      background-color="#424242"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      router
+    >
+      <el-menu-item index="/">Home</el-menu-item>
+      <el-menu-item index="/about">About</el-menu-item>
+    </el-menu>
 
-  <router-view />
-  <el-backtop />
+    <router-view />
+  </el-scrollbar>
 </template>
 
 <script setup lang="ts">
