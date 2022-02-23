92876ef41b019703290929bf44782a1e84a8d245
TTQ <1932170807@qq.com>
2022-02-23 22:47:09 +0800

    GameView get game detail

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,13 +1,27 @@
 <template>
   <div>
-    <span>{{ id }}</span>
+    <span>{{ data }}</span>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps } from "vue";
+import getInfoService from "@/services/getInfoService.js";
+import { ref, defineProps, onMounted } from "vue";
 
-defineProps<{
+const props = defineProps<{
   id: string;
 }>();
+
+const data = ref();
+
+const getData = async () => {
+  data.value = await getInfoService.getInfo({
+    id: props.id,
+  });
+  console.log(data.value);
+};
+
+onMounted(() => {
+  getData();
+});
 </script>
