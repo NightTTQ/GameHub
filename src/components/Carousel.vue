9ed24948bb51a6aead3ab3bf314db297226419f8
TTQ <1932170807@qq.com>
2022-03-05 16:02:38 +0800

    switch buttons

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="carousel-detail"></div>
+  <div class="carousel-detail">
+    <div class="hover-container">
+      <div class="switch left">
+        <button class="switch-btn" @click="doSwitch(-1)">
+          <el-icon :size="20" color="#FFFFFF"><arrow-left-bold /></el-icon>
+        </button>
+      </div>
+
+      <div class="switch right">
+        <button class="switch-btn" @click="doSwitch(1)">
+          <el-icon :size="20" color="#FFFFFF"><arrow-right-bold /></el-icon>
+        </button>
+      </div>
+    </div>
+    <ul class="detail-slider"></ul>
+  </div>
   <div class="carousel-swiper">
     <ul class="carousel-swiper-wrapper">
       <li v-for="(item, index) in data" class="carousel-swiper-item">
@@ -23,6 +38,7 @@
 
 <script lang="ts" setup>
 import { ref, computed, onMounted } from "vue";
+import { ArrowLeftBold, ArrowRightBold } from "@element-plus/icons-vue";
 const props = defineProps<{
   data?: Array<Image>;
 }>();
@@ -33,8 +49,8 @@ interface Image {
 
 const current = ref(0);
 
-const toLeft = () => {
-  console.log("go Left!");
+const doSwitch = (dir: number) => {
+  console.log(dir);
 };
 const switchTo = (index: number) => {
   console.log(index);
@@ -45,9 +61,59 @@ onMounted(() => {
 </script>
 
 <style scoped>
+.switch {
+  color: #fff;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: 0;
+  background: linear-gradient(
+    270deg,
+    rgba(11, 11, 11, 0) 0%,
+    rgba(11, 11, 11, 0.4) 100%
+  );
+  transition: transform 0.35s cubic-bezier(1, 0.09, 0.03, 0.93),
+    opacity 0.35s ease;
+}
+.left {
+  transform: translateX(-100%);
+}
+.right {
+  transform: translateX(100%);
+}
+@media (min-width: 1440px) {
+  .switch {
+    width: 60px;
+  }
+}
 .carousel-detail {
   background-color: black;
+  position: relative;
+  overflow: hidden;
   padding-top: calc(9 / 16 * 100%);
+}
+.hover-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  pointer-events: none;
+  z-index: 1;
+}
+.carousel-detail:hover .switch {
+  transform: translateX(0);
+  opacity: 1;
+}
+.switch-btn {
+  border: none;
+  padding: 10px;
+  pointer-events: auto;
+  position: relative;
 }
 .carousel-swiper {
   width: 100%;
