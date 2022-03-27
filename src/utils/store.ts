35bdc1610ed16b7a89d7696c2a85cc4c31753493
TTQ <1932170807@qq.com>
2022-03-27 16:04:17 +0800

    Token相关的存储功能

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,4 +1,6 @@
 const localSessionKey = "SessionID";
+const localTokenKey = "TOKEN";
+const localRefreshTokenKey = "RefreshToken";
 
 export default {
   /**
@@ -18,6 +20,43 @@ export default {
    * @desc 删除localSession
    */
   removeSession() {
-    return localStorage.removeItem(localSessionKey);
+    localStorage.removeItem(localSessionKey);
+  },
+  /**
+   * @desc 获取localToken
+   */
+  getToken() {
+    return localStorage.getItem(localTokenKey);
+  },
+  /**
+   * @desc 更改localToken
+   * @param value 更改的值
+   */
+  setToken(value: string) {
+    localStorage.setItem(localTokenKey, value);
+  },
+  /**
+   * @desc 删除localToken
+   */
+  removeToken() {
+    localStorage.removeItem(localTokenKey);
+  },
+  /**
+   * @desc 获取localRefreshToken
+   */ getRefreshToken() {
+    return localStorage.getItem(localRefreshTokenKey);
+  },
+  /**
+   * @desc 更改localRefreshToken
+   * @param value 更改的值
+   */
+  setRefreshToken(value: string) {
+    localStorage.setItem(localRefreshTokenKey, value);
+  },
+  /**
+   * @desc 删除localRefreshToken
+   */
+  removeRefreshToken() {
+    localStorage.removeItem(localRefreshTokenKey);
   },
 };
