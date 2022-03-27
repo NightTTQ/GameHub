e4ae7ffccccbc18bf6821cfb8e35b535bc99ebcd
TTQ <1932170807@qq.com>
2022-03-27 17:49:10 +0800

    上传权限认证

--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
+import store from "@/utils/store";
 const apiLink = "https://qcnnig.api.cloudendpoint.cn/uploadFile";
 const apiConfig = {
-  headers: { "Content-Type": "multipart/form-data" },
+  headers: {
+    "Content-Type": "multipart/form-data",
+    "x-tt-session-v2": store.getSession()!,
+    TOKEN: store.getToken()!,
+  },
 };
 
 export default {
