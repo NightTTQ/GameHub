299c208551bfbf75647e3ac806c5176a016af831
TTQ <1932170807@qq.com>
2022-04-02 20:37:15 +0800

    修复路由检测登录状态错误的问题

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from "pinia";
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-import { useUserStore } from "@/stores";
+import store from "@/utils/store";
 import "video.js/dist/video-js.css";
 
 import App from "./App.vue";
@@ -15,9 +15,8 @@ const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
 router.beforeEach((to, from, next) => {
-  const user = useUserStore();
-
-  if (user.isLogin) {
+  // 只要本地存在token则路由认为已登录
+  if (store.getToken()) {
     //用户已登录不能进入登录和注册页
     if (to.name === "login" || to.name === "register") {
       next({ name: "home" });
