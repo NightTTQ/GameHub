984e73002c09fd28b5078ca6a72ccf42427fd4c1
TTQ <1932170807@qq.com>
2022-04-02 20:06:25 +0800

    获取属于用户的游戏列表

---
+++ src/services/getUserGameService.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import store from "@/utils/store";
+const apiLink = "https://qcnnig.api.cloudendpoint.cn/getMyGame";
+const apiConfig = {
+  headers: {
+    "Content-Type": "multipart/form-data",
+    "x-tt-session-v2": store.getSession()!,
+    TOKEN: store.getToken()!,
+  },
+};
+
+export default {
+  async getGame() {
+    const params = {};
+    const { data } = await axios
+      .post(apiLink, params, apiConfig)
+      .then((response) => {
+        return response;
+      })
+      .catch((error) => {
+        return error;
+      });
+    return data;
+  },
+};
