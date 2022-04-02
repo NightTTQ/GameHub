189fe2220223f1d2893b73f8641f2544c33e94c6
TTQ <1932170807@qq.com>
2022-04-02 22:39:52 +0800

    修复请求token不更新的问题

--- src/services/getUserGameService.ts
+++ src/services/getUserGameService.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 import store from "@/utils/store";
 const apiLink = "https://qcnnig.api.cloudendpoint.cn/getMyGame";
-const apiConfig = {
-  headers: {
-    "Content-Type": "multipart/form-data",
-    "x-tt-session-v2": store.getSession()!,
-    TOKEN: store.getToken()!,
-  },
-};
 
 export default {
   async getGame() {
+    const apiConfig = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        "x-tt-session-v2": store.getSession()!,
+        TOKEN: store.getToken()!,
+      },
+    };
     const params = {};
     const { data } = await axios
       .post(apiLink, params, apiConfig)
--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 import store from "@/utils/store";
 const apiLink = "https://qcnnig.api.cloudendpoint.cn/uploadFile";
-const apiConfig = {
-  headers: {
-    "Content-Type": "multipart/form-data",
-    "x-tt-session-v2": store.getSession()!,
-    TOKEN: store.getToken()!,
-  },
-};
 
 export default {
   async upload(file: File) {
+    const apiConfig = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        "x-tt-session-v2": store.getSession()!,
+        TOKEN: store.getToken()!,
+      },
+    };
     // 构建 FormData 对象
     const _FormData = new FormData();
     // 添加文件
