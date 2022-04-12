d0aa47a9ea18b5a2678ec9d93087cc8b2b46770f
TTQ <1932170807@qq.com>
2022-04-12 22:54:30 +0800

    重构

--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -1,14 +1,10 @@
-import axios from "axios";
-import store from "@/utils/store";
-const apiLink = "https://qcnnig.api.cloudendpoint.cn/uploadFile";
+import request from "@/services/axios/request";
 
 export default {
   async upload(file: File) {
-    const apiConfig = {
+    const config = {
       headers: {
         "Content-Type": "multipart/form-data",
-        "x-tt-session-v2": store.getSession()!,
-        TOKEN: store.getToken()!,
       },
     };
     // 构建 FormData 对象
@@ -17,14 +13,7 @@ export default {
     _FormData.append("file", file);
 
     // return params;
-    const { data } = await axios
-      .post(apiLink, _FormData, apiConfig)
-      .then((response) => {
-        return response;
-      })
-      .catch((error) => {
-        return error;
-      });
+    const { data } = await request.post("/uploadFile", _FormData, config);
     return data;
   },
 };
