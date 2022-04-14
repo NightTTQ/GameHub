12f6c65eb43c559992fc66964614c7dd1bf3469e
TTQ <1932170807@qq.com>
2022-04-14 10:53:25 +0800

    实现无感双token刷新

--- src/services/axios/request.ts
+++ src/services/axios/request.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 import store from "@/utils/store";
+import { refreshToken } from "@/services/userService";
+
+// 控制变量
+let isRefreshing = false;
+let queue: Array<Function> = [];
 
 // Axios实例
 const instance = axios.create({
@@ -23,16 +28,43 @@ instance.interceptors.request.use(
 // 响应拦截器
 instance.interceptors.response.use(
   (res) => {
-    console.log(res);
+    // 返回数据提示token过期，刷新token
+    if (res.data.code == 1002) {
+      const config = res.config;
+      // token过期需要刷新
+      console.log("token is expired");
+      if (!isRefreshing) {
+        // 当前未在刷新，开始刷新
+        return refreshToken()
+          .then(() => {
+            // 刷新token成功
+            // 刷新配置
+            config.headers!["token"] = store.getToken()!;
+            // 刷新完成开始对队列中请求进行重试
+            queue.forEach((rq) => rq());
+            queue = [];
+            return instance(config);
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+          .finally(() => (isRefreshing = false));
+      } else {
+        // 当前正在刷新，请求加入队列
+        return new Promise((resolve) => {
+          queue.push(() => {
+            // 刷新配置
+            config.headers!["token"] = store.getToken()!;
+            resolve(instance(config));
+          });
+        });
+      }
+    }
     return res;
   },
   (err) => {
     return Promise.reject(err);
   }
 );
-
-function getLocalToken() {
-  return store.getToken();
-}
 
 export default instance;
