4e738c63d24cdabb7ba7025559f17c7e904af3f2
TTQ <1932170807@qq.com>
2022-05-03 23:03:27 +0800

    加入测试代码

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button @click="">Test</el-button>
   </div>
 </template>
+<script setup lang="ts">
+import Dialog from "@/utils/dialog";
 
-<style>
+const test = () => {};
+</script>
+<style lang="scss" scoped>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
